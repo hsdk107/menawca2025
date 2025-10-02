8d164c56ff9388bb27b875b9f4128e35495662a9
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 17:00:15 +0400

    Update program.py

--- program.py
+++ program.py
@@ -13,8 +13,8 @@ with day1:
     st.write("**Breakfast & Registration**")
     st.write("Outside Blue Hall, Arts Center (C3)")
     st.write("*Registration will continue past this time if needed at the Library Entrance (C2)*")
-
-  st.divider()
+st.markdown("<hr style='margin:0;'>")
+  #st.divider()
 
   col0, col1 = st.columns([2,5],gap="small")
   with col0:

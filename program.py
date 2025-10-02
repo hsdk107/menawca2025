96cc314b967ea1178bfbf05eacf87cf10c0aa6e2
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 22:36:55 +0400

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-chart_data = st.session_state.chart_data
 st.header("Conference Program")
 
 day1, day2, day3 = st.tabs(["**Friday October 10**", "**Saturday, October 11**", "**Sunday, October 12**"])

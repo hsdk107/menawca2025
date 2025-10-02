efd53c046c3f2ddf78e54261c98dc195f7186c3a
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 16:42:25 +0400

    Update program.py

--- program.py
+++ program.py
@@ -9,10 +9,11 @@ with day1:
   col0, col1 = st.columns([1,3],gap="small")
   with col0:
     with st.container(border=True):
-      st.code(
-        "9:00-10:00 am\n"
-        "(Registration will continue past this time if needed)"
-      )
+      st.info("9:00-10:00 am\n\n(Registration will continue past this time if needed)")
+      #st.code(
+      #  "9:00-10:00 am\n"
+      #  "(Registration will continue past this time if needed)"
+      #)
   with col1:
     st.write("**Breakfast & Registration**")
     st.write("Outside Blue Hall, Arts Center (C3)")

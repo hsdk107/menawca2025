3a4d0f0bc8c9b517f5de1aea6b24caa966cca29e
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 21:21:07 +0400

    Update home.py

--- home.py
+++ home.py
@@ -8,13 +8,25 @@ import streamlit as st
 st.title("MENAWCA 2025")
 st.header("Co-creation with AI: Navigating New Horizons in Writing and Learning")
 
+st.markdown('''
+MENAWCA’s 9th biennial conference is a platform for instructors, tutors, peer tutors, center directors, researchers 
+and other professionals supporting student writers in the MENA region, to share innovative practices and research.''')
+st.write("This year's conference is hosted at [New York University Abu Dhabi](https://nyuad.nyu.edu/en/) from *10 to 12 October, 2025*.")
 
 st.markdown('''
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed elit sed dui pulvinar tincidunt.
-	Integer consequat turpis sit amet orci ullamcorper consequat. Duis volutpat sodales volutpat. 
-	Morbi mattis metus at diam tempor efficitur vitae et purus. Ut sit amet justo a mauris vulputate auctor. 
-	Duis hendrerit aliquet leo. Proin ut dui eget diam aliquet aliquet. Sed elementum euismod nibh at posuere. 
-	Mauris malesuada tortor et congue interdum. Donec pulvinar tortor commodo tristique volutpat.''')
+	Writing centers and programs focus on the writer, encouraging agency, empowerment, 
+	and self-actualization through developing not only composition but also linguistic skills. 
+	Traditionally, this collaboration exists as a relationship between the writer and the reader/tutor/instructor; 
+	however, as generative AI (GenAI) becomes increasingly present and pervasive in composition, 
+	students are turning to it for support. While some are engaging with GenAI creatively and critically, 
+	many writers often rely on these tools to generate text. As such, writing centers 
+	and writing programs are now at a crossroads of praxis, determining when, where, 
+	and to what extent GenAI should be part of composition moving forward. 
+	These considerations add to the existing complexities and nuances of academic writing, especially in the MENA region. 
+	Where it is essential to consider the ways in which writers can continue to develop their own voices, 
+	it is also becoming necessary to factor in GenAI. 
+	As practitioners, we can discuss methods and share stories to figure out how to use GenAI as a tool, 
+	moving toward co-creation rather than dependence.''')
 
 col0, col1, col2 = st.columns(3, border=True)
 

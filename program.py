01afcb8abcca0a66045aacc523cfbc2bce721b22
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 16:06:47 +0400

    Update program.py

--- program.py
+++ program.py
@@ -5,9 +5,11 @@ st.header("Conference Program")
 
 day1, day2, day3 = st.tabs(["Friday October 10", "Saturday, October 11", "Sunday, October 12"])
 with day1:
+  st.header("This is day 1")
   st.table(chart_data)
 with day2:
-  st.write("This is an expander")
+  st.header("This is day 2")
   st.table(chart_data)
 with day3:
+  st.header("This is day 3")
   st.table(chart_data)

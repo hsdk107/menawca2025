8aa5ce83d4619b781d1e917277c4f68d300772b2
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 10:45:57 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,22 +44,22 @@ pages = [
 page = st.navigation(pages)
 page.run()
 
-'''with st.sidebar.container(height=310):
-	if page.title == "Widgets":
-		widgets_card()
-	elif page.title == "Data":
-		dataframe_card()
-	elif page.title == "Layouts":
-		layouts_card()
-	else:
-		st.page_link("home.py", label="Home", icon=":material/home:")
-		st.write("Welcome to the home page!")
-		st.write(
-			"Select a page from above. This sidebar thumbnail shows a subset of "
-			"elements from each page so you can see the sidebar theme."
-		)
+#with st.sidebar.container(height=310):
+#	if page.title == "Widgets":
+#		widgets_card()
+#	elif page.title == "Data":
+#		dataframe_card()
+#	elif page.title == "Layouts":
+#		layouts_card()
+#	else:
+#		st.page_link("home.py", label="Home", icon=":material/home:")
+#		st.write("Welcome to the home page!")
+#		st.write(
+#			"Select a page from above. This sidebar thumbnail shows a subset of "
+#			"elements from each page so you can see the sidebar theme."
+#		)
 
-st.sidebar.caption(
-	"This app uses [Space Grotesk](https://fonts.google.com/specimen/Space+Grotesk) "
-	"and [Space Mono](https://fonts.google.com/specimen/Space+Mono) fonts."
-)'''
+#st.sidebar.caption(
+#	"This app uses [Space Grotesk](https://fonts.google.com/specimen/Space+Grotesk) "
+#	"and [Space Mono](https://fonts.google.com/specimen/Space+Mono) fonts."
+#)

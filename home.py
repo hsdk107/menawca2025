3b09f3dda1671c4ae72bd7bae8d64eac882e6154
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 21:26:08 +0400

    Update home.py

--- home.py
+++ home.py
@@ -8,9 +8,8 @@ import streamlit as st
 st.title("MENAWCA 2025")
 st.header("Co-creation with AI: Navigating New Horizons in Writing and Learning")
 
-st.markdown('''
-MENAWCA’s 9th biennial conference is a platform for instructors, tutors, peer tutors, center directors, researchers 
-and other professionals supporting student writers in the MENA region, to share innovative practices and research.''')
+st.write("[MENAWCA’s](https://menawca.com/) 9th biennial conference is a platform for instructors, tutors, peer tutors, center directors, researchers 
+and other professionals supporting student writers in the MENA region, to share innovative practices and research.")
 st.write("This year's conference is hosted at [New York University Abu Dhabi](https://nyuad.nyu.edu/en/) from **10 to 12 October, 2025**.")
 
 st.markdown('''

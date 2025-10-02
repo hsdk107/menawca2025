a72ee29570d1775299e90a02b10f00e3ecaadd33
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 15:58:45 +0400

    Create program.py

---
+++ program.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+chart_data = st.session_state.chart_data
+st.header("Conference Program")
+
+st.subheading("Friday, October 10 2025")
+st.table(chart_data)
+
+st.divider()
+
+with st.expander("Saturday, October 11 2025"):
+  st.write("This is an expander")
+  st.table(chart_data)
+
+st.divider()
+
+st.subheading("Sunday, October 12 2025")
+st.table(chart_data)

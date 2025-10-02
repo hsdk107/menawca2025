ebf09acc00392969ac4a5e93c96d116a4cf8f281
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 11:21:45 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from cards import (
-	widgets_card,
-	dataframe_card,
-	layouts_card
-)
+#from cards import (
+#	widgets_card,
+#	dataframe_card,
+#	layouts_card
+#)
 
 if "init" not in st.session_state:
 	st.session_state.chart_data = pd.DataFrame(
 		np.random.randn(20, 3), columns=["a", "b", "c"]
 	)
-	st.session_state.map_data = pd.DataFrame(
-		np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-		columns=["lat", "lon"],
-	)
 	st.session_state.init = True
-
 
 pages = [
 	st.Page(

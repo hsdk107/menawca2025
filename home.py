b363020934cac4546fd846263cdc57e8a8f32468
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 10:14:55 +0400

    Add files via upload

---
+++ home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from cards import (
+	widgets_card,
+	text_card,
+	dataframe_card,
+	charts_card,
+	media_card,
+	layouts_card,
+	chat_card,
+	status_card
+)
+
+st.title("Streamlit element explorer")
+
+st.markdown(
+	"This app displays most of Streamlit's built-in elements so you can "
+	"conveniently explore how they look with different theming configurations "
+	"applied."
+)
+
+cols = st.columns(2)
+with cols[0].container(height=310):
+	widgets_card()
+with cols[0].container(height=310):
+	dataframe_card()
+with cols[1].container(height=310):
+	layouts_card()

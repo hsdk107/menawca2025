9f1d2687091ab0dfd900293dab9cc0df9fa982ea
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 21:50:26 +0400

    Update abstracts.py

--- abstracts.py
+++ abstracts.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.header("Session Speakers and Abstracts")
 
- with st.expander("Parallel Sessions",expanded=True):
-   st.write("TBD")
+with st.expander("Parallel Sessions",expanded=True):
+ st.write("TBD")

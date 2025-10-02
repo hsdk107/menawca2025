fc6098434c7f86d9e5771151e3c70c6292735ff8
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 16:30:28 +0400

    Update program.py

--- program.py
+++ program.py
@@ -6,6 +6,15 @@ st.header("Conference Program")
 day1, day2, day3 = st.tabs(["**Friday October 10**", "**Saturday, October 11**", "**Sunday, October 12**"])
 with day1:
   st.header("Day 1: Friday October 10, 2025")
+  col0, col1 = st.columns([1,3],gap="small")
+  with col0:
+    st.write("9:00 am - 10:00 am")
+    st.write("(Registration will continue past this time if needed")
+  with col1:
+    st.write("**Breakfast & Registration**")
+    st.write("Outside Blue Hall, Arts Center (C3)")
+    st.write("Registration will continue at the Library Entrance (C2)")
+
   st.table(chart_data)
 with day2:
   st.header("Day 2: Saturday October 11 2025")

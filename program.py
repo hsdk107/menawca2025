44ca5895494bc4f30d38ba4210aef5cc7f3b9f10
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 16:47:58 +0400

    Update program.py

--- program.py
+++ program.py
@@ -16,7 +16,7 @@ with day1:
   with col1:
     st.write("**Breakfast & Registration**")
     st.write("Outside Blue Hall, Arts Center (C3)")
-    st.write("Registration will continue past this time if needed at the Library Entrance (C2)")
+    st.write("*Registration will continue past this time if needed at the Library Entrance (C2)*")
 
   st.table(chart_data)
 with day2:

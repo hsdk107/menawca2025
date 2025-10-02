17b8036412fef4ea815898f45a8d11f3d1aae1ff
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-02 17:26:11 +0400

    Update program.py

--- program.py
+++ program.py
@@ -4,9 +4,14 @@ chart_data = st.session_state.chart_data
 st.header("Conference Program")
 
 day1, day2, day3 = st.tabs(["**Friday October 10**", "**Saturday, October 11**", "**Sunday, October 12**"])
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# ******************* FRIDAY SCHEDULE *******************************
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 with day1:
   st.header("Day 1: Friday October 10, 2025")
   st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
   col0, col1 = st.columns([2,5],gap="small")
   with col0:
     st.write("9:00 - 10:00am")
@@ -15,6 +20,7 @@ with day1:
     st.write("Outside Blue Hall, Arts Center (C3)")
     st.write("*Registration will continue past this time if needed at the Library Entrance (C2)*")
   st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
   col0, col1 = st.columns([2,5],gap="small")
   with col0:
     st.write("10:00am - 12:00pm")
@@ -43,6 +49,7 @@ with day1:
   with col1:
     st.write("Provost's Welcome Address")
   st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
   col0, col1 = st.columns([2,5],gap="small")
   with col0:
     st.write("12:00 - 2:30pm")
@@ -51,10 +58,38 @@ with day1:
     st.write("Torch Club (D2)")
     st.write("*Prayer Rooms in C2 Ground Floor*")
   st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
+  col0, col1 = st.columns([2,5],gap="small")
+  with col0:
+    st.write("2:30 - 5:30pm")
+  with col1:
+    st.write("**Policy, Voice and Agency**")
+    st.write("Campus Center (C2) Level 3, Library, Breakout Rooms")
+  st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
+  col0, col1, col2, col3 = st.columns([1,2,2,2],gap="small")
+  with col0:
+    st.write("Room")
+  with col1:
+    st.error("TBD")
+  with col2:
+    st.error("TBD")
+  with col3:
+    st.error("TBD")
+  st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
+  # *****************************************************************************************************
   st.table(chart_data)
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# ******************* SATURDAY SCHEDULE *******************************
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 with day2:
   st.header("Day 2: Saturday October 11 2025")
   st.table(chart_data)
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# ******************* SUNDAY SCHEDULE *******************************
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 with day3:
   st.header("Day 3: Sunday October 12 2025")
   st.table(chart_data)

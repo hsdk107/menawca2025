83def6ceca9afdf81dfff6a34188aee01eabe397
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-08 08:37:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,13 @@ import pandas as pd
 import numpy as np
 
 st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
+	"""
+	<style>
+	#MainMenu {visibility: hidden;}
+	</style>
+	""",
+	unsafe_allow_html=True
 )
-
 pages = [
 	st.Page(
 		"home.py",

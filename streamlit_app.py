186c00d4f8ef6b4c1aef79d604df37341aabf627
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-08 08:33:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 pages = [
 	st.Page(
 		"home.py",

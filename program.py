d71d6b290969b8dc4841a72e11969becdf2cb8ae
hsdk107 <144232645+hsdk107@users.noreply.github.com>
2025-10-09 17:01:34 +0400

    Update program.py

--- program.py
+++ program.py
@@ -54,7 +54,9 @@ with day1:
       #st.markdown('<div style="text-align: right;">11:45am-12:00pm</div>', unsafe_allow_html=True)
       st.write("11:45am-12:00pm")
     with col1:
-      st.write("Provost's Welcome Address: Arlie Petters")
+      st.write("Leadership Welcome Address")
+      st.write("Arlie Petters, Provost")
+      st.write("Awam Amkpa, Vice Provost for the Arts, Dean of Arts and Humanities")
   st.markdown("<hr style='margin:0;'>",unsafe_allow_html=True)
   # *****************************************************************************************************
   col0, col1 = st.columns([2,5],gap="small")
